ebff3844418020b44f7e2d3c7b2fa0fba9e2d7da
Diogo Condêço <diogobcondeco@gmail.com>
2024-02-29 20:14:32 +0000

    adding a blog page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
-  const test="test"
-  const another="another"
+  const test = "test";
+  const another = "another";
+  const thirdwheel = "third";
 
   return <div>Hello World, diogo is here.</div>;
 }
