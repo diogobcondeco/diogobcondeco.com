830c65ed7b1b38a51b69663091c1c390cdd9bc5b
Diogo Condêço <diogobcondeco@gmail.com>
2024-03-01 00:12:07 +0000

    install analytics and fix speed insights

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import Main from "@/components/Main";
 
 export default function Home() {
   return (
     <>
-      <SpeedInsights />
       <Main />
     </>
   );
