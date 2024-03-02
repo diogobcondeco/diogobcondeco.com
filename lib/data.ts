3f4276b580f660d94eb4da5719fe274f384ef32a
Diogo Condêço <diogobcondeco@gmail.com>
2024-03-02 16:49:28 +0000

    added skills section

--- lib/data.ts
+++ lib/data.ts
@@ -9,8 +9,9 @@ import techprowlImg from "@/public/techprowl-two.png";
 import taskManagerImg from "@/public/task-manager.png";
 import familyTreeImg from "@/public/bfsproject.jpg";
 import binaryGameImg from "@/public//binary-game.png";
+import { Link, Skill } from "./types";
 
-export const links = [
+export const links: Link[] = [
   {
     name: "Home",
     hash: "#home",
@@ -35,4 +36,83 @@ export const links = [
     name: "Contact",
     hash: "#contact",
   },
+] as const;
+
+export const skillsData: Skill[] = [
+  {
+    name: "JavaScript",
+    icon: "logos:javascript",
+  },
+  {
+    name: "TypeScript",
+    icon: "logos:typescript-icon",
+  },
+  {
+    name: "React",
+    icon: "logos:react",
+  },
+  {
+    name: "Node.js",
+    icon: "logos:nodejs-icon",
+  },
+  {
+    name: "PHP",
+    icon: "logos:php",
+  },
+  // {
+  //   name: "Laravel",
+  //   icon: "logos:laravel",
+  // },
+  // {
+  //   name: "Symfony",
+  //   icon: "logos:symfony",
+  // },
+  {
+    name: "Python",
+    icon: "logos:python",
+  },
+  {
+    name: "Java",
+    icon: "logos:java",
+  },
+  {
+    name: "HTML",
+    icon: "vscode-icons:file-type-html",
+  },
+  {
+    name: "CSS",
+    icon: "vscode-icons:file-type-css",
+  },
+  {
+    name: "SCSS",
+    icon: "logos:sass",
+  },
+  {
+    name: "Git",
+    icon: "logos:git-icon",
+  },
+  {
+    name: "MySQL",
+    icon: "logos:mysql",
+  },
+  {
+    name: "Linux",
+    icon: "flat-color-icons:linux",
+  },
+  // {
+  //   name: "AWS",
+  //   icon: "logos:aws",
+  // },
+  {
+    name: "Docker",
+    icon: "logos:docker-icon",
+  },
+  {
+    name: "Jenkins",
+    icon: "devicon:jenkins",
+  },
+  // {
+  //   name: "Shopify",
+  //   icon: "logos:shopify",
+  // },
 ] as const;
--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,13 @@
 import { links } from "./data";
 
 export type SectionName = (typeof links)[number]["name"];
+
+export type Link = {
+  name: string;
+  hash: string;
+};
+
+export type Skill = {
+  name: string;
+  icon: string;
+};
