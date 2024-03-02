70fe2d46d99774013f6ee3797f23272ab9dbb4c1
Diogo Condêço <diogobcondeco@gmail.com>
2024-03-02 16:43:15 +0000

    added intro section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Intro from "@/components/Intro";
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
+      <Intro />
       <div className="flex flex-col items-center px-4"></div>
     </main>
   );
